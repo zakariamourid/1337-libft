c6029714868ca4ecfa74be7434ebbbf5bd94d6de
Zakariae Mourid <zmourid@c4r1p7.1337.ma>
2023-11-17 19:36:53 +0100

    split 3

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: zmourid <zmourid@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/11 23:18:30 by zmourid           #+#    #+#             */
-/*   Updated: 2023/11/17 19:32:42 by zmourid          ###   ########.fr       */
+/*   Updated: 2023/11/17 19:36:45 by zmourid          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,6 +52,8 @@ char	**ft_split(char const *s, char c)
 	char	**str;
 
 	len = count_words(s, c);
+	if(!count_words)
+		return (NULL);
 	i = 0;
 	str = (char **)malloc(sizeof(char *) * (len+ 1));
 	if(!str)
